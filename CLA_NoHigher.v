0ff7159a3bfd5eafc954de9e73f656ee433ec435
AymanAkhter <cs21b012@smail.iitm.ac.in>
2023-02-06 15:22:59 +0530

    Changed CLA_NoHigher to support subtraction

--- CLA_NoHigher.v
+++ CLA_NoHigher.v
@@ -1,9 +1,9 @@
 `timescale 1ns/1ns
 
-module CLA_NoHigher(result,OF,operand1,operand2,add,sign);
+module CLA_NoHigher(result,OF,operand1,operand2,sub,sign);
 
     input[15:0] operand1, operand2;
-    input add, sign;
+    input sub, sign;
     output[15:0] result;
     output OF;
 
@@ -11,12 +11,17 @@ module CLA_NoHigher(result,OF,operand1,operand2,add,sign);
     wire[3:0] carries;
     wire[3:0] c2l;
 
+    wire[15:0] operand3;
+    xor Xors[15:0] (operand3[15:0], operand2[15:0], sub);
+
     not Not[15:0] (operand2_bar[15:0], operand2[15:0]);
 
-    CLA c1 (result[3:0], carries[0], c2l[0], operand1[3:0], operand2[3:0], add);
-    CLA c2 (result[7:4], carries[1], c2l[1], operand1[7:4], operand2[7:4], carries[0]);
-    CLA c3 (result[11:8], carries[2], c2l[2], operand1[11:8], operand2[11:8], carries[1]);
-    CLA c4 (result[15:12], carries[3], c2l[3], operand1[15:12], operand2[15:12], carries[2]);
+    CLA c1 (result[3:0], carries[0], c2l[0], operand1[3:0], operand3[3:0], sub);
+    CLA c2 (result[7:4], carries[1], c2l[1], operand1[7:4], operand3[7:4], carries[0]);
+    CLA c3 (result[11:8], carries[2], c2l[2], operand1[11:8], operand3[11:8], carries[1]);
+    CLA c4 (result[15:12], carries[3], c2l[3], operand1[15:12], operand3[15:12], carries[2]);
+
+
 
     wire o1,o2;
     buf Buf1 (o1,carries[3]);
@@ -27,7 +32,7 @@ module CLA_NoHigher(result,OF,operand1,operand2,add,sign);
 
     wire mint1, mint2;
     and And1 (mint1, sign_bar, o1);
-    and And2 (mint2, sing,  o2);
+    and And2 (mint2, sign,  o2);
 
     or Or (OF, mint1, mint2);
 
