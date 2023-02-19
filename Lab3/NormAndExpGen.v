6765e76b73ed412b6314c25134a2e2a9e14b6ac4
Snehadeep Gayen <cs21b078@smail.iitm.ac.in>
2023-02-20 01:58:52 +0530

    Completed Normal and Exponent Generator module

--- Lab3/NormAndExpGen.v
+++ Lab3/NormAndExpGen.v
@@ -9,16 +9,21 @@ module NormandExpGen (
     input wire [3:0] Xe, Ye,
     input wire [8:0] sum
 );
-    wire [3:0] Ge, GePlus;
+    wire [3:0] Ge/*, GePlus*/;
     MUX m[3:0] (Ge, XeLTYe, Xe, Ye);
 
-    supply1 __high;
-    supply0 __low;
-    buf (high, __high);
-    buf (low, __low);
-    RCA8bit r (GePlus, temp_cout, Ge, low, high);
+    // supply1 __high;
+    // supply0 __low;
+    // buf (high, __high);
+    // buf (low, __low);
+    // RCA8bit r (GePlus, temp_cout, Ge, low, high);
 
-    wire [3:0] LDZshift
+    wire [2:0] LDZshift;
+    wire [7:0] BarrelOut;
     PEncoder P (sum[7:0], LDZshift);
-    BarrelShift BB
+    BarrelShift BB(BarrelOut, sum[7:0], LDZshift);
+
+    MUX m [6:0] (Zm, sum[8], BarrelOut[6:0], sum[6:0]);
+
+    RBS4bit BB (ans, temp_borrow, Ge, LDZshift);
 endmodule
