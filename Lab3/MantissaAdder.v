fcbdb5f3bfb3fe303364b4b8582fc9c8ea0759e0
AymanAkhter <cs21b012@smail.iitm.ac.in>
2023-02-20 01:48:22 +0530

    Added MantissaAdder

--- Lab3/MantissaAdder.v
+++ Lab3/MantissaAdder.v
@@ -5,9 +5,17 @@ module MantissaAdder(Sm, Pm, Qm, MAS);
     input[7:0] Pm, Qm;
     input MAS;
     output[8:0] Sm;
+    supply0 zero;
 
     wire[8:0] sum, diff;
+    wire cout, bout;
 
-    
+    RCA8bit RC(sum[7:0], cout, Pm[7:0], Qm[7:0], zero);
+    RBS8bit RB(diff[7:0], bout, Pm[7:0], Qm[7:0], zero);
+
+    sum[8] = cout;
+    diff[8] = zero;
+
+    MUX m[8:0] (Sm[8:0], MAS, sum[8:0], diff[8:0]);
 
 endmodule
