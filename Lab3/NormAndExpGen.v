ba71851cfaf59e0df627454f0106ed19cbd79115
Snehadeep Gayen <cs21b078@smail.iitm.ac.in>
2023-02-20 15:25:37 +0530

    Norm And Exp Gen changed

--- Lab3/NormAndExpGen.v
+++ Lab3/NormAndExpGen.v
@@ -12,14 +12,16 @@ module NormandExpGen (
     input wire [3:0] Xe, Ye,
     input wire [8:0] sum
 );
-    wire [3:0] Ge/*, GePlus*/;
+    wire [3:0] Ge, GePlus;
     MUX m[3:0] (Ge, XeLTYe, Xe, Ye);
 
     // supply1 __high;
     supply0 __low;
     // buf (high, __high);
     buf (low, __low);
-    RCA8bit r (GePlus, temp_cout, Ge, low, sum[8]);
+    wire [3:0] all_low;
+    buf pullitdown [3:0] (all_low, low);
+    RCA4bit r (GePlus, temp_cout, Ge, all_low, sum[8]);
 
     wire [3:0] LDZshift;
     buf (LDZshift[3], low);
@@ -28,7 +30,7 @@ module NormandExpGen (
 
     BarrelShift BB(BarrelOut, sum[7:0], LDZshift[2:0]);
 
-    MUX mm [6:0] (Zm, sum[8], BarrelOut[6:0], sum[6:0]);
+    MUX mm [6:0] (Zm, sum[8], BarrelOut[6:0], sum[7:1]);
 
     RBS4bit RR (Ze, temp_borrow, Ge, LDZshift, low);
 endmodule
