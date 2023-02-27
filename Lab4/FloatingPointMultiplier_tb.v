b60388f035f85de10b540e937ebc96f3c8bdca83
AymanAkhter <cs21b012@smail.iitm.ac.in>
2023-02-27 14:55:42 +0530

    TestBench, changes

--- Lab4/FloatingPointMultiplier_tb.v
+++ Lab4/FloatingPointMultiplier_tb.v
@@ -4,8 +4,9 @@ module FloatingMultiplier_tb();
 
 	reg[11:0] A,B;
     wire[11:0] result;
+    wire overflow;
 
-    FloatingPointMultiplier F (A, B, result);
+    FloatingPointMultiplier F (A, B, result, overflow);
 	
 	initial begin
 		
@@ -17,48 +18,48 @@ module FloatingMultiplier_tb();
         B = 12'b010011000000;
 
         #20
-        $display("x = %b, y = %b\n     result = %b", x, y, result);
-        $display("Ans should be 011000101000\n");
+        $display("x = %b, y = %b\n     result = %b, Overflow = %b", A, B, result, overflow);
+        $display("Ans should be 001010101000 , Overflow = 0\n");
 
         //Case 2 : No Overflow, Diff sign, Pm greater than 2.0
 		A = 12'b000101100000;
         B = 12'b110011000000;
 
         #20
-        $display("x = %b, y = %b\n     result = %b", x, y, result);
-        $display("Ans should be 110100010000\n");
+        $display("x = %b, y = %b\n     result = %b, Overflow = %b", A, B, result, overflow);
+        $display("Ans should be 101010101000 , Overflow = 0\n");
 
         //Case 3 : No Overflow, Same sign, Pm less than 2.0
 		A = 12'b010101000000;
         B = 12'b000110100000;
 
         #20
-        $display("x = %b, y = %b\n     result = %b", x, y, result);
-        $display("Ans should be 011011110000\n");
+        $display("x = %b, y = %b\n     result = %b, Overflow = %b", A, B, result, overflow);
+        $display("Ans should be 001101110000 , Overflow = 0\n");
 
         //Case 4 : No Overflow, Diff sign, Pm less than 2.0
 		A = 12'b110101000000;
         B = 12'b000110100000;
 
         #20
-        $display("x = %b, y = %b\n     result = %b", x, y, result);
-        $display("Ans should be 110011110000\n");
+        $display("x = %b, y = %b\n     result = %b, Overflow = %b", A, B, result, overflow);
+        $display("Ans should be 101101110000 , Overflow = 0\n");
 
         //Case 5 : Exponent Positive Overflow
-		x = 12'b011001000000;
-        y = 12'b010110100000;
+		A = 12'b011001000000;
+        B = 12'b010110100000;
 
         #20
-        $display("x = %b, y = %b\n     result = %b", x, y, result);
-        $display("Ans should be 000001110000\n");
+        $display("x = %b, y = %b\n     result = %b, Overflow = %b", A, B, result, overflow);
+        $display("Ans should be 000001110000 , Overflow = 1\n");
 
         //Case 6 : Exponent Negative overflow
-		x = 12'b100111000000;
-        y = 12'b100100100000;
+		A = 12'b100111000000;
+        B = 12'b100100100000;
 
         #20
-        $display("x = %b, y = %b\n     result = %b", x, y, result);
-        $display("Ans should be 011101110000\n");
+        $display("x = %b, y = %b\n     result = %b, Overflow = %b", A, B, result, overflow);
+        $display("Ans should be 011101110000 , Overflow = 1\n");
 
 		
 	end
